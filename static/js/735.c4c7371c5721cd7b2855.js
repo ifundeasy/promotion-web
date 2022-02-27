6b752f47c1b5eefed22fe9c6ebbd171dbdf69c37
wkylin.w <wkylin.w@gmail.com>
2022-02-27 21:05:21 +0800

    Updates

---
+++ static/js/735.c4c7371c5721cd7b2855.js
@@ -0,0 +1,49 @@
+"use strict";
+(self["webpackChunkpromotion_web"] = self["webpackChunkpromotion_web"] || []).push([[735],{
+
+/***/ 3994:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7294);
+
+
+var FixTabPanel = function FixTabPanel(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    style: {
+      width: '100%',
+      minHeight: 'calc(100vh - 242px)'
+    }
+  }, children);
+};
+
+/* harmony default export */ __webpack_exports__["Z"] = (FixTabPanel);
+
+/***/ }),
+
+/***/ 5735:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7294);
+/* harmony import */ var _stateless_FixTabPanel__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3994);
+
+
+
+var Error = function Error() {
+  var error = {
+    error: 'error'
+  };
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_stateless_FixTabPanel__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", null, "Cool! Hi, React && Ant Design"), error.map(function (item) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+      key: item
+    }, item);
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Error);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=735.c4c7371c5721cd7b2855.js.map
---
+++ static/js/996.4dc841b3e355765f5149.js
@@ -0,0 +1,42 @@
+"use strict";
+(self["webpackChunkpromotion_web"] = self["webpackChunkpromotion_web"] || []).push([[996],{
+
+/***/ 3994:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7294);
+
+
+var FixTabPanel = function FixTabPanel(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    style: {
+      width: '100%',
+      minHeight: 'calc(100vh - 242px)'
+    }
+  }, children);
+};
+
+/* harmony default export */ __webpack_exports__["Z"] = (FixTabPanel);
+
+/***/ }),
+
+/***/ 8996:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7294);
+/* harmony import */ var _stateless_FixTabPanel__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3994);
+
+
+
+var Home = function Home() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_stateless_FixTabPanel__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", null, "Cool! Hi, React && Ant Design"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, "React version: ", react__WEBPACK_IMPORTED_MODULE_0__.version));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=996.4dc841b3e355765f5149.js.map
